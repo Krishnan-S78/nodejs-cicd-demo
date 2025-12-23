58f7617ee681d156ed2d8ebe55790ea8b21e88e4
Sanjai Sen <sensanjai957@gmail.com>
2025-12-23 15:30:30 +0000

    Add Node.js app with fixed test script

---
+++ app.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const os = require('os');
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Hello DevOps from SanjaiSen on ${new Date()}`);
+}).listen(3000, () => console.log('App running on port 3000'));
+
