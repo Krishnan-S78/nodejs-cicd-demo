5a16f8ad432226d743a72b9c4dd95fb55dd41b32
Krishnan-S78 <sensanjai957@gmail.com>
2026-02-07 01:59:35 +0530

    Update response message in app.js
    Signed-off-by: Krishnan-S78 <sensanjai957@gmail.com>

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const http = require('http');
 const os = require('os');
 http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(`Hello DevOps from SanjaiSen on ${new Date()}`);
+  res.end(`Hello DevOps0 from SanjaiSen on ${new Date()}`);
 }).listen(3000, () => console.log('App running on port 3000'));
 
